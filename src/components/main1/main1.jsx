404038d8528389b8d83a2a1a2e79e9b8827e60ba
sagarpathak0 <112740255+sagarpathak0@users.noreply.github.com>
2024-06-04 06:41:13 +0530

    Update main1.jsx

--- src/components/main1/main1.jsx
+++ src/components/main1/main1.jsx
@@ -1,27 +1,25 @@
-import './main1.css'
-import video1 from '../../assets/edit2024.mp4'
-import image1 from '../../assets/p1.jpg'
-import image2 from '../../assets/12.png'
-import image3 from '../../assets/11.jpeg'
+import './main1.css';
+import video1 from '../../assets/edit2024.mp4';
+import image1 from '../../assets/p1.jpg';
+import image2 from '../../assets/12.png';
+import image3 from '../../assets/11.jpeg';
 
-const main1 = ({playStatus, heroCount}) => {
+const Main1 = ({ playStatus, heroCount }) => {
 
-    if(playStatus){
-        return(
+    if (playStatus) {
+        return (
             <video className='background' autoPlay loop muted>
                 <source src={video1} type='video/mp4' />
             </video>
-        )
+        );
+    } else if (heroCount === 0) {
+        return <img src={image1} className='background' alt='Background' />;
+    } else if (heroCount === 1) {
+        return <img src={image2} className='background' alt='Background' />;
+    } else if (heroCount === 2) {
+        return <img src={image3} className='background' alt='Background' />;
     }
-    else if(heroCount===0){
-        return <img src={image1} className='background' alt='not found'/>
-    }
-    else if(heroCount===1){
-        return <img src={image2} className='background' alt='not found'/>
-    }
-    else if(heroCount===2){
-        return <img src={image3} className='background' alt='not found'/>
-    }
-}
+    return null;
+};
 
-export default main1;
+export default Main1;
